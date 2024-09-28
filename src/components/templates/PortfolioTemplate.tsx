e3852a8f82d3e9f4106ca1e6f7c34d40d66f4e18
Sebastian Saldarriaga <ssaldarriaga.cardona@udea.edu.co>
2024-09-28 14:53:27 -0500

    refactor: clean project for update

--- src/components/templates/PortfolioTemplate.tsx
+++ src/components/templates/PortfolioTemplate.tsx
@@ -1,22 +1,7 @@
 import React from "react";
-import MainContent from "../organisms/MainContent";
-import SocialLinks from "../organisms/SocialLinks";
-import LeftColumn from "../organisms/LeftColumn";
 
 const PortfolioTemplate: React.FC = () => {
-  return (
-    <div className="flex flex-col lg:flex-row">
-      <div className="w-full lg:w-[20%] lg:order-first">
-        <LeftColumn />
-      </div>
-      <div className="w-full lg:w-[75%]">
-        <MainContent />
-      </div>
-      <div className="w-full lg:w-[5%] lg:order-last lg:fixed lg:top-0 lg:right-0">
-        <SocialLinks />
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default PortfolioTemplate;
